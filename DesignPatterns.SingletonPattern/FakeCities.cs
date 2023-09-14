f517f6c1e132c149da54422324cf79bd46d6ccd7
sinancansevinc <sinancan_sevinc@hotmail.com>
2023-09-14 16:03:11 +0300

    Singleton pattern updated.

---
+++ DesignPatterns.SingletonPattern/FakeCities.cs
@@ -0,0 +1,54 @@
+﻿using DesignPatterns.SingletonPattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SingletonPattern
+{
+    public class FakeCities
+    {
+        private static FakeCities instance = null;
+        public static FakeCities Instance
+        {
+            get
+            {
+                if (instance is not null)
+                {
+                    // return existed
+                    return instance;
+                }
+                else
+                {
+                    instance = new FakeCities();
+                    return instance;
+                }
+
+
+            }
+
+            set => instance = value;
+
+        }
+
+        public async Task<List<City>> GetCities()
+        {
+            await Task.Delay(2000);
+            return new List<City>()
+            {
+                new City() { Name="Istanbul"},
+                new City() { Name = "London"},
+                new City() { Name="Berlin"}
+            };
+        }
+
+        public async Task<City> GetCity()
+        {
+            return new City
+            {
+                Name = "New York"
+            };
+        }
+    }
+}
--- DesignPatterns.SingletonPattern/Program.cs
+++ DesignPatterns.SingletonPattern/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Pros:
+    - Single instance
+    - Global access
+    - Create when requested. After the first request , object will be created.
+
+Cons:
+    - UnitTest is difficult when mocking
+*/
+
+using DesignPatterns.SingletonPattern;
+
+var cities = await FakeCities.Instance.GetCities();
+
+var anotherCities = await FakeCities.Instance.GetCity();
+
+Console.ReadLine();
+//When another service calling , it is created again and again and it took 4 second.
